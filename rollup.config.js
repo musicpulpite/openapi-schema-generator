b1300e9966b73584c7521259a10b71b2572bdb52
musicpulpite <musicpulpite@gmail.com>
2023-08-27 11:35:33 -0400

    fix: add standard rollup plugins to handle node core package imports and commonjs/esm interop

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
-// using https://www.npmjs.com/package/@rollup/plugin-typescript
 import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: './src/index.ts',
@@ -8,5 +9,5 @@ export default {
     format: 'cjs',
     banner: '#!/usr/bin/env node',
   },
-  plugins: [typescript()],
+  plugins: [typescript(), nodeResolve(), commonjs()],
 };
